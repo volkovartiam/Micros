6d77f1a66888584307a6d940a484f60c14bf3d7b
MineScieper <volkov_artiam@mail.ru>
2025-02-27 09:20:40 +0400

    Arduino LCD DS1307

--- Arduino/8_9_RTC_DS3231/README.txt
+++ Arduino/8_9_RTC_DS3231/README.txt
@@ -6,3 +6,11 @@ https://kit.alexgyver.ru/tutorials/ds3231/?ysclid=m7hvn1zul8338594322
 https://microkontroller.ru/arduino-projects/podklyuchenie-modulya-chasov-realnogo-vremeni-ds3231-k-arduino/?ysclid=m7hvn48ssi436368086
 
 https://reference.arduino.cc/reference/en/libraries/ds3231/
+
+
+https://github.com/bonezegei/Bonezegei_DS1307
+
+https://deepbluembedded.com/stm32-blue-pill-pinout-programming-guide/
+
+
+
--- Arduino/8_9_RTC_DS3231/RTC_DS1307/RTC_DS1307.ino
+++ Arduino/8_9_RTC_DS3231/RTC_DS1307/RTC_DS1307.ino
@@ -3,8 +3,7 @@
   Uno       A4 (SDA), A5 (SCL)
 */
 
-#include <Wire.h>
-#include <DS1307.h>
+#include <Bonezegei_DS1307.h>
 #include <LiquidCrystal.h>
 
 // Номера пинов Arduino, к которым подключен дисплей
@@ -16,52 +15,34 @@ constexpr uint8_t LCD_RS_PIN = 8;
 constexpr uint8_t LCD_EN_PIN = 9;
 constexpr uint8_t LCD_RW_PIN = 10;
 
-// Создаем объект класса LiquidCrystal
+
 LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_DB4_PIN, LCD_DB5_PIN, LCD_DB6_PIN, LCD_DB7_PIN);
 
-DS1307 rtc; // Initialize the RTC
-
+Bonezegei_DS1307 rtc(0x68);
 
 int delayMs = 250;
 
 
-
 void setup()
 {
-  /*
-  //init Serial port
-  Serial.begin(9600);
-  lcd.init();                      // initialize the lcd
-  while (!Serial); //wait for serial port to connect - needed for Leonardo only
-
-  //init RTC
-  Serial.println("Init RTC...");
-  */
-
+  
   // Начинаем работу с дисплеем
   lcd.begin(16, 2);
   // Устанавливаем положение курсора, 0-й столбец, 0-я строка
   lcd.setCursor(0, 0);
   // Выводим текст
-  lcd.print("MicroTechnics");
+  lcd.print("Arduino is a power");
   // Устанавливаем положение курсора, 0-й столбец, 1-я строка
   lcd.setCursor(0, 1);
   // Выводим еще немного текста, уже на другой строке
   lcd.print("Hi there, LCD!");
 
-
-  /**/
   rtc.begin();
-
-  //only set the date+time one time
-  //rtc.set(0, 0, 8, 17, 04, 2021); //08:00:00 24.12.2014 //sec, min, hour, day, month, year
-
-  //stop/pause RTC
-  // rtc.stop();
-
-  //start RTC
-  rtc.start();
-  /**/
+  /*
+  rtc.setFormat(24);        //Set 12 Hours Format
+  rtc.setTime("8:55:15");  //Set Time    Hour:Minute:Seconds
+  rtc.setDate("2/27/25");   //Set Date    Month/Date/Year
+  */
 }
 
 
@@ -71,32 +52,15 @@ void loop()
   uint8_t sec, min, hour, day, month;
   uint16_t year;
 
-  //get time from RTC
-  rtc.get(&sec, &min, &hour, &day, &month, &year);
-
-  /*
-  //serial output
-  Serial.print("nTime: ");
-  Serial.print(hour, DEC);
-  Serial.print(":");
-  Serial.print(min, DEC);
-  Serial.print(":");
-  Serial.print(sec, DEC);
-  Serial.print("nDate: ");
-  Serial.print(day, DEC);
-  Serial.print(".");
-  Serial.print(month, DEC);
-  Serial.print(".");
-  Serial.print(year, DEC);
-  // set cursor to first line
-  */
-  lcd.setCursor(5, 0);
-  lcd.backlight();
-  lcd.print("Hello!");
-  lcd.setCursor(1, 1);
-  lcd.print("MICRODIGISOFT");
-  //wait a second
-  delay(10);
+  if(rtc.getTime() ) {
+    sec = rtc.getSeconds();
+    min = rtc.getMinute();
+    hour = rtc.getHour();
+    
+    day = rtc.getDate();
+    month = rtc.getMonth();
+    year = rtc.getYear();
+  }
 
   lcd.clear();
   lcd.setCursor(1, 0);
@@ -106,6 +70,7 @@ void loop()
   lcd.print(month);
   lcd.print("/");
   lcd.print(year);
+
   lcd.setCursor(1, 1);
   lcd.print("Time:");
   lcd.print(hour);
@@ -113,9 +78,9 @@ void loop()
   lcd.print(min);
   lcd.print(":");
   lcd.print(sec);
-  delay(1000);
+  delay(100);
   lcd.clear();
-
+/**/
 }
 
 
@@ -124,62 +89,3 @@ void loop()
 
 
 
-
-
-
-
-
-
-
-/*
-
-void setup() 
-{  
-  // Начинаем работу с дисплеем
-  lcd.begin(16, 2);
-  // Устанавливаем положение курсора, 0-й столбец, 0-я строка
-  lcd.setCursor(0, 0);
-  // Выводим текст
-  lcd.print("MicroTechnics");
-  // Устанавливаем положение курсора, 0-й столбец, 1-я строка
-  lcd.setCursor(0, 1);
-  // Выводим еще немного текста, уже на другой строке
-  lcd.print("Hi there, LCD!");
-}
-void loop() 
-{
-  delay(delayMs);
-  lcd.clear();
-  lcd.home();
-  delay(delayMs);
-  lcd.write("Hello world");
-
-  delay(delayMs);
-  lcd.cursor();
-  //lcd.noCursor();
-
-  delay(delayMs);
-  lcd.blink();
-  //lcd.noBlink();
-
-  delay(delayMs);
-  lcd.noDisplay();
-  delay(delayMs);
-  lcd.display();
-
-  delay(delayMs);
-  lcd.home();
-  //lcd.scrollDisplayLeft();
-  lcd.scrollDisplayRight();
-  lcd.scrollDisplayRight();
-  lcd.scrollDisplayRight();
-  lcd.write("Hello world");
-  delay(delayMs);
-
-//  lcd.leftToRight();
-  lcd.rightToLeft();
-  lcd.write("Welcome");
-
-
-}
-*/
