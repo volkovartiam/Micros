c5487336b5341045f1a26c765463f48b0716056f
MineScieper <volkov_artiam@mail.ru>
2025-05-05 10:19:38 +0400

    ADC

--- Atmega/22_ADC/ADC_Atmega328P/ADC_Interrupt/ADC_Interrupt/main.c
+++ Atmega/22_ADC/ADC_Atmega328P/ADC_Interrupt/ADC_Interrupt/main.c
@@ -1,17 +1,16 @@
 /*
- * ADC.c
+ * ADC_Interrupt.c
  *
- * Created: 04.05.2025 20:43:16
- * Author : Artem
+ * Created: 05.05.2025 9:39:01
+ * Author : 589
  */ 
-
 
 
 /*
 ADMUX - ADC Multiplexer selection register
-REFS1, REFS0 - voltage reference selection	
+REFS1, REFS0 - voltage reference selection
 ADLAR - ADC left adjust result
-MUX3, MUX2, MUX1, MUX0 - analog channel selection bits 
+MUX3, MUX2, MUX1, MUX0 - analog channel selection bits
 
 ADCSRA - ADC Control and status register A
 ADEN - ADC Enable
@@ -35,6 +34,7 @@ ADC5D, ADC4D, ADC3D, ADC2D, ADC1D, ADC0D - digital input disable
 #define Internal_1_1_V (1<<REFS1)|(1<<REFS0)
 
 #define ADC0 (0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0)
+#define ADC1 (0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(1<<MUX0)
 #define ADC8_TEMP (1<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0)
 #define V_BG (1<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0)
 
@@ -44,25 +44,25 @@ ADC5D, ADC4D, ADC3D, ADC2D, ADC1D, ADC0D - digital input disable
 
 #include "usart.h"
 
+char high_adc=0;
+char low_adc=0;
 
 int main(void)
 {
-	USART_Init (BAUD_9600);	
+	USART_Init (BAUD_9600);
 	ADC_Init();
 	//ADC_Init_Temp();
+	sei();
 	
 	int adc_value = 0;
 	int adch_value = 0;
 
-    while (1) 
-    {
-		adc_value = ADC_convert();
-		adch_value = adc_value>>8;
-		USART_Transmit(adch_value);
-		USART_Transmit(adc_value);		
-		
+	while (1)
+	{
+		USART_Transmit(high_adc);
+		USART_Transmit(low_adc);
 		_delay_ms(500);
-    }
+	}
 }
 
 void USART_OK()
@@ -77,19 +77,25 @@ void USART_OK()
 void ADC_Init(void)
 {
 	ADCSRA |= (1<<ADEN)| 						// Разрешение использования АЦП
-			//(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)
-			  DIV_Factor_128;					//Делитель 128 = 64 кГц
-	ADMUX |= AVcc_VOLTAGE;			//Внутренний Источник ОН 2,56в, вход ADC0
+	//(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)
+			  DIV_Factor_128|					//Делитель 128 = 64 кГц
+			  (1<<ADSC)|						//Запуск преобразования
+			  (1<<ADATE)|						//Непрерывный режим работы АЦП
+			  (1<<ADIE);						//Разрешение прерывания от АЦП
+			  			  
+	ADMUX |= AVcc_VOLTAGE|ADC0;	
+	//ADMUX |= AVcc_VOLTAGE|ADC8_TEMP;
+	//ADMUX |= AVcc_VOLTAGE|V_BG;	
+		
 }
 
 
 void ADC_Init_Temp(void)
 {
 	ADCSRA |= (1<<ADEN)| 						// Разрешение использования АЦП
-			  DIV_Factor_128;					//Делитель 128 = 64 кГц
-	ADMUX |= Internal_1_1_V|ADC8_TEMP;			//Внутренний Источник ОН 2,56в, вход ADC0
+	DIV_Factor_128;								//Делитель 128 = 64 кГц
+	ADMUX |= Internal_1_1_V|ADC8_TEMP;			//Внутренний Источник ОН 1,1В
 }
-
 
 
 int ADC_convert(void)
@@ -98,3 +104,11 @@ int ADC_convert(void)
 	while((ADCSRA & (1<<ADSC)));	//проверим закончилось ли аналого-цифровое преобразование
 	return ADC;
 }
+
+ISR(ADC_vect)
+{
+	low_adc = ADCL;
+	high_adc = ADCH;				//Верхняя часть регистра ADC должна быть считана последней, иначе не продолжится преобразование
+	//adc_value = high_adc*256+low_adc;
+}
+//---
---
+++ Atmega/22_ADC/ADC_Atmega328P/ADC_Interrupt/ADC_Interrupt/usart.h
@@ -0,0 +1,37 @@
+/*
+ * usart.h
+ *
+ * Created: 04.04.2025 21:33:53
+ *  Author: Artem
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+
+//Скорость передачи зависит от тактовой частоты (для 16МГц см. стр.165)
+// И регистра удвоения тактовой частоты 
+//Задается регистром UBRRN
+#define BAUD_2400 416
+#define BAUD_4800 207
+#define BAUD_9600 103
+#define BAUD_14400 68
+#define BAUD_19200 51
+#define BAUD_28800 34
+#define BAUD_38_4K 25
+#define BAUD_57_6K 16
+#define BAUD_76_8K 12
+#define BAUD_115_2K 8
+#define BAUD_230_4K 3
+#define BAUD_250K 3
+#define BAUD_0_5K 1
+#define BAUD_1M 0
+
+
+#include "main.h"
+
+void USART_Init( unsigned int speed);
+void USART_Transmit( unsigned char data );
+
+
+#endif /* USART_H_ */
