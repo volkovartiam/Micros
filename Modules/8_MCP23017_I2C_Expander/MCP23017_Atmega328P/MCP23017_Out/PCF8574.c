c984da5d0dfb1b5a25c73210ab9b18144684f197
MineScieper <volkov_artiam@mail.ru>
2025-05-20 09:23:41 +0400

    MCP23017

---
+++ Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/PCF8574.c
@@ -0,0 +1,66 @@
+/*
+ * PCF8574.c
+ *
+ * Created: 13.05.2025 8:43:08
+ *  Author: 589
+ */ 
+
+#include "PCF8574.h"
+
+
+void init_int(){
+	DDRB |= (1<<PORTB5);
+
+	PORTB |= (1<<PORTB5);
+	
+	/*
+	_delay_ms(2000);
+	
+	PORTB &= ~(1<<PORTB5);
+	_delay_ms(2000);
+	
+	PORTB |= (1<<PORTB5);
+	_delay_ms(2000);
+	*/
+}
+
+
+void pulse_int(){
+	PORTB |= (1<<PORTB5);
+	//_delay_ms(1);
+	
+	PORTB &= ~(1<<PORTB5);
+	//_delay_ms(1);
+	
+	/*
+	PORTB |= (1<<PORTB5);
+	//_delay_ms(1);
+	/**/
+}
+
+
+
+/*
+void setAddressPCF8574(unsigned char address){
+	#define PCF8574_ADDRESS address;
+}
+*/
+
+void set_output_PCF8574(unsigned char data){
+	I2C_Init();
+	I2C_Start();
+	I2C_Send_Data(I2C_PCF_ADDRESS_WITH_WRITE);
+	I2C_Send_Data(data);
+	I2C_Stop();
+}
+
+
+unsigned char get_input_PCF8574(){
+	unsigned char dataFromInput = 0;
+	I2C_Init();
+	I2C_Start();	
+	I2C_Send_Data(I2C_PCF_ADDRESS_WITH_READ);
+	dataFromInput = I2C_Read_Data(0);
+	I2C_Stop();
+	return dataFromInput;
+}
---
+++ Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/twi.c
@@ -0,0 +1,73 @@
+﻿#include "twi.h"
+
+
+void I2C_Init(void)
+{
+	TWBR = BIT_RATE;
+	init_telemetry();
+	send_telemetry(I2C_STATUS);
+}
+
+
+void I2C_Start(void)
+{
+	TWCR = TWI_Interrupt_Flag|TWI_START_Condition|TWI_Enable;
+	while(WAIT_UNTIL_TWINT_SET){}
+	send_telemetry(I2C_STATUS);
+}
+
+
+void I2C_Stop(void)
+{
+	TWCR = TWI_Interrupt_Flag|TWI_STOP_Condition|TWI_Enable;
+	send_telemetry(I2C_STATUS);
+}
+
+
+void I2C_Send_Data(unsigned char data)
+{
+	TWDR = data;					
+	TWCR = TWI_Interrupt_Flag|TWI_Enable;
+	while(WAIT_UNTIL_TWINT_SET){}
+	send_telemetry(I2C_STATUS);
+}
+
+
+char err1=0;// сюда вернем ошибку
+#define TW_MR_DATA_ASK 0x50 // Ведущий принял данные и передал подтверждение
+
+unsigned char I2C_Read_Data(uint8_t END)
+{
+	/**/
+	if(END == 1){
+		TWCR = (1<<TWINT)|(1<<TWEN);
+	} else{
+		TWCR = TWI_Interrupt_Flag|TWI_Enable|TWI_Enable_Acknowledge;
+	}
+	while(WAIT_UNTIL_TWINT_SET){}		
+	send_telemetry(TWDR);							
+	return TWDR;
+	/**/
+	
+	/*
+	err1=0;
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);//включим прием данных
+	while(!(TWCR & (1<<TWINT)));//подождем пока установится TWIN
+	if ((TWSR & 0xF8) != TW_MR_DATA_ASK)  err1=1;
+	else err1=0;
+	return TWDR;
+	*/	
+}
+
+
+void init_telemetry()
+{
+	USART_Init();
+}
+
+void send_telemetry(uint8_t data)
+{
+	USART_Transmit(data);
+}
+
+
