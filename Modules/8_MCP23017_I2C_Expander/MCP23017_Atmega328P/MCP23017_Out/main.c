b1cf8cff9a01b5a70db5d9040a809bd4ed08791f
MineScieper <volkov_artiam@mail.ru>
2025-05-21 09:22:38 +0400

    MCP23017 Output Input Byte Mode

--- Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/main.c
+++ Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/main.c
@@ -14,61 +14,101 @@
 #define ADDRESS_WITH_READ PCF8574_ADDRESS|READ
 #define ADDRESS_WITH_WRITE PCF8574_ADDRESS|WRITE
 
+
+void turn_all_HIGH()
+{
+		//BANK 0
+		I2C_Init();
+		I2C_Start();
+		I2C_Send_Data(ADDRESS_WITH_WRITE);
+		I2C_Send_Data(0x00);	// IODIRA
+		I2C_Send_Data(0x00);	// sets all port A pins to outputs
+		I2C_Stop();
+		
+		I2C_Init();
+		I2C_Start();
+		I2C_Send_Data(ADDRESS_WITH_WRITE);
+		I2C_Send_Data(0x01);	// IODIRB
+		I2C_Send_Data(0x00);	// sets all port B pins to outputs
+		I2C_Stop();
+		
+		I2C_Init();
+		I2C_Start();
+		I2C_Send_Data(ADDRESS_WITH_WRITE);
+		I2C_Send_Data(0x12);	// 
+		I2C_Send_Data(0xFF);	// sets all port to HIGH
+		I2C_Stop();
+	
+		I2C_Init();
+		I2C_Start();
+		I2C_Send_Data(ADDRESS_WITH_WRITE);
+		I2C_Send_Data(0x13);	//
+		I2C_Send_Data(0xFF);	// sets all port to HIGH
+		I2C_Stop();
+}
+
+
+void turn_A_HIGH_B_LOW()
+{
+	I2C_Init();
+	I2C_Start();
+	I2C_Send_Data(ADDRESS_WITH_WRITE);
+	I2C_Send_Data(0x00);	// IODIRA
+	I2C_Send_Data(0x00);	// sets all port A pins to outputs
+	I2C_Stop();
+			
+	I2C_Init();
+	I2C_Start();
+	I2C_Send_Data(ADDRESS_WITH_WRITE);
+	I2C_Send_Data(0x01);	// IODIRB
+	I2C_Send_Data(0x00);	// sets all port B pins to outputs
+	I2C_Stop();
+			
+	I2C_Init();
+	I2C_Start();
+	I2C_Send_Data(ADDRESS_WITH_WRITE);
+	I2C_Send_Data(0x12);			//
+	//I2C_Send_Data(0x00);			// sets all port to HIGH
+	I2C_Send_Data(0b11111100);		// 
+	I2C_Stop();
+			
+	I2C_Init();
+	I2C_Start();
+	I2C_Send_Data(ADDRESS_WITH_WRITE);
+	I2C_Send_Data(0x13);	//
+	I2C_Send_Data(0xFF);	// sets all port to HIGH
+	I2C_Stop();
+}
+
 int main(void)
 {
 	uint32_t delay = 2500; 
+	uint8_t data = 0;
 
     while(1)
     {
-		/*	
-		I2C_Init();
-		I2C_Start();
-		I2C_Send_Data(ADDRESS_WITH_WRITE);
-		I2C_Send_Data(0x00);	// IODIR
-		I2C_Stop();
-		
-		I2C_Start();
-		I2C_Send_Data(ADDRESS_WITH_WRITE);
-		I2C_Send_Data(0x00);	
-		I2C_Stop();
-		
-		
-		I2C_Send_Data(0x00);
-		
-		I2C_Send_Data(0x06);	// GPPUA
-		I2C_Send_Data(0xFF);
 	
-		I2C_Start();
-		I2C_Send_Data(ADDRESS_WITH_WRITE);
-		I2C_Send_Data(0x09);	// GPIO
-		I2C_Stop();
-		
-		I2C_Start();
-		I2C_Send_Data(ADDRESS_WITH_WRITE);
-		I2C_Send_Data(0xFF);		
-		I2C_Stop();
-		//USART_Transmit(data);
-		_delay_ms(delay);
-		/**/
-		
-		/**/
+		//turn_all_HIGH();	
+		turn_A_HIGH_B_LOW();
+	
 		I2C_Init();
 		I2C_Start();
 		I2C_Send_Data(ADDRESS_WITH_WRITE);
-		/*
-		I2C_Send_Data(0x00);	// IODIR
-		I2C_Send_Data(0x00);
-				
-		I2C_Send_Data(0x06);	// GPPUA
-		I2C_Send_Data(0xFF);
-		*/
-		I2C_Send_Data(0x09);	// GPIO
-		I2C_Send_Data(0xFF);
-		I2C_Stop();
-		/**/		
+		I2C_Send_Data(0x12);			//
 
+		I2C_Start();
+		I2C_Send_Data(ADDRESS_WITH_READ);
+		data = I2C_Read_Data(1);
+		I2C_Stop();
+
+		USART_Transmit(data);
 		_delay_ms(delay);
+
 	}	
 	
 }
 
+/*
+	Sequantional mode
+	IOCON register, INPUT
+*/
--- Modules/8_MCP23017_I2C_Expander/README.txt
+++ Modules/8_MCP23017_I2C_Expander/README.txt
@@ -1,4 +1,6 @@
 https://www.learningaboutelectronics.com/Articles/MCP23017-IO-port-expander-circuit-with-arduino.php
+https://www.learningaboutelectronics.com/Calculators/
+
 https://www.arduinolearning.com/code/arduino-and-mcp23017-example.php
 
 https://www.woolseyworkshop.com/2021/03/18/adding-digital-io-to-your-arduino-part-3-the-mcp23017/
@@ -12,3 +14,5 @@ https://www.instructables.com/Adding-an-MCP23017-IO-Extender-to-Arduino-or-ESP82
 https://www.makerhacks.com/mcp23017/
 https://mysku.club/blog/aliexpress/59294.html
 https://robotclass.ru/tutorials/roc-mcp23017/?ysclid=maw2941ro5583754585
+
+https://github.com/alkonosst/MCP23017?ysclid=maxgyebd6x165513449
