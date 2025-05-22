84785cd8badfa88332ac434f434c139024c2592c
MineScieper <volkov_artiam@mail.ru>
2025-05-22 09:14:01 +0400

    MCP23017_Input

--- Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/Debug/main.d
+++ Modules/8_MCP23017_I2C_Expander/MCP23017_Atmega328P/MCP23017_Out/Debug/main.d
@@ -18,7 +18,7 @@ main.d main.o: .././main.c .././main.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- .././usart.h .././twi.h .././PCF8574.h
+ .././usart.h .././twi.h
 
 .././main.h:
 
@@ -63,5 +63,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 .././usart.h:
 
 .././twi.h:
-
-.././PCF8574.h:
